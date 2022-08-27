719d0aa6f7874e0f3f2af6dca3eb95e7d50695f1
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-08-27 23:14:07 +0800

    存档

--- classInit/classInit/learning/java/jvm/ClassInit.java
+++ classInit/classInit/learning/java/jvm/ClassInit.java
@@ -4,5 +4,34 @@ package learning.java.jvm;
  * @author 禤成伟
  * @date 2022-08-27 - 23:11
  */
+class ParentClass {
+    private int parentX = 10;
+    public ParentClass() {
+        setX(100);
+    }
+    public void setX(int x) {
+        parentX = x;
+        System.out.println("ParentX 被赋值为 " + x);
+    }
+}
+class ChildClass extends ParentClass{
+    private int childX = 1;
+    public ChildClass() {}
+    @Override
+    public void setX(int x) {
+        childX = x;
+        System.out.println("ChildX  被赋值为 " + x);
+    }
+
+    public void printX(){
+        System.out.println("ChildX  的值是   " + childX);
+    }
+}
+
 public class ClassInit {
+    public static void main(String[] args) {
+        ParentClass parentClass = new ParentClass();
+        ChildClass childClass = new ChildClass();
+        childClass.printX();
+    }
 }
