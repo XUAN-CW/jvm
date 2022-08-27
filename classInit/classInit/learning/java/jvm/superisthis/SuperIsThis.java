3c1676c06f6422980988bff059d96cebf72dc218
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-08-28 00:21:53 +0800

    存档

---
+++ classInit/classInit/learning/java/jvm/superisthis/SuperIsThis.java
@@ -0,0 +1,25 @@
+package learning.java.jvm.superisthis;
+
+/**
+ * @author 禤成伟
+ * @date 2022-08-27 - 23:14
+ */
+class Superclass {
+    Superclass(){
+        System.out.println(this);
+    }
+}
+class Subclass extends Superclass {
+    public Subclass() {
+        System.out.println(this);
+
+    }
+}
+
+
+public class SuperIsThis {
+    public static void main(String[] args) {
+        new Subclass();
+        new Superclass();
+    }
+}
