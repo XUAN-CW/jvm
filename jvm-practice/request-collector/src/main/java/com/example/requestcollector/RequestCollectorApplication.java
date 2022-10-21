1a67cfc80f02466b122d799e966199110973067e
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-10-22 00:32:04 +0800

    INIT

---
+++ jvm-practice/request-collector/src/main/java/com/example/requestcollector/RequestCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestCollectorApplication.class, args);
+    }
+
+}
---
+++ jvm-practice/request-collector/src/test/java/com/example/requestcollector/RequestCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.requestcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
